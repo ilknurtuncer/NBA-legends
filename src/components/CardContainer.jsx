57d874b13634dd560968df894fd5d353e9dcf8bb
Ilknur Tuncer <ilknurtuncer67@gmail.com>
2023-02-21 21:05:08 +0300

    import data

---
+++ src/components/CardContainer.jsx
@@ -0,0 +1,10 @@
+import Container from "react-bootstrap/Container";
+import { data } from "../helpers/data"
+
+const CardContainer = () => {
+  return (
+    <div>CardContainer</div>
+  )
+}
+
+export default CardContainer
